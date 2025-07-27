f3183ff8486540086964b1f3d58b50d19832f5cc
Philip Thomas <thomasphil6889@gmail.com>
2025-07-28 01:31:57 +0530

    updated the app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 app  = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -46,7 +47,8 @@ def delete(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 
 
